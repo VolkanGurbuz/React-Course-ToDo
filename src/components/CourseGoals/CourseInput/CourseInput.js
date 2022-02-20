41b50263c420337a7f23fd81618c65c68e78f89d
volkangurbuz <volkangurbuz16@gmail.com>
2022-02-20 11:24:24 +0100

    css classes added dynamically with input of user

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,20 +5,30 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+      //dynamic inline styles added with enter user input
+     const[isValid, setIsValud] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0){
+      setIsValud(true);
+    }
+
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length ===0){
+      setIsValud(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label >Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
