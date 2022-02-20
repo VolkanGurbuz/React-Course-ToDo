07699e93b626d2372bca0fe3c16972efb46e3db0
volkangurbuz <volkangurbuz16@gmail.com>
2022-02-20 14:10:39 +0100

    media queries addded

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -12,6 +12,10 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+  width: 100%;
+  @media (min-width:768){
+  width:auto;
+  }
 
 
 &:focus {
